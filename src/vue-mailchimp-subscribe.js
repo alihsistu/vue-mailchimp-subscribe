8e5d18ce5d3bf79dc190aae49c451e0c45f5ce2d
Ali Hosseini <alihsistu@gmail.com>
2020-07-17 11:58:21 +0800

    added some new fields

--- src/vue-mailchimp-subscribe.js
+++ src/vue-mailchimp-subscribe.js
@@ -2,93 +2,110 @@ import jsonp from 'jsonp'
 import queryString from 'query-string'
 
 export default {
-  props: {
-    url: {
-      required: true,
-      type: String,
+    mounted() {
+        console.log('Mounted')
+    },
+    props: {
+        url: {
+            required: true,
+            type: String,
+        },
+
+        userId: {
+            required: true,
+            type: String,
+        },
+
+        listId: {
+            required: true,
+            type: String,
+        },
     },
 
-    userId: {
-      required: true,
-      type: String,
-    },
-
-    listId: {
-      required: true,
-      type: String,
-    },
-  },
-
-  data() {
-    return {
-      email: null,
-      success: false,
-      error: null,
-      loading: false,
-    }
-  },
-
-  computed: {
     data() {
-      return queryString.stringify({
-        u: this.userId,
-        id: this.listId,
-        EMAIL: this.email,
-      })
-    },
-  },
-
-  methods: {
-    setEmail(value = '') {
-      this.email = value.trim()
+        return {
+            email: null,
+            name: null,
+            message: null,
+            success: false,
+            error: null,
+            loading: false,
+        }
     },
 
-    subscribe() {
-      if (this.email === null || this.loading) {
-        return
-      }
-
-      this.success = false
-      this.error = null
-      this.loading = true
-
-      const url = `${this.url}?${this.data}`
-
-      jsonp(url, { param: 'c' }, this.onResponse)
+    computed: {
+        data() {
+            return queryString.stringify({
+                u: this.userId,
+                id: this.listId,
+                EMAIL: this.email,
+                FNAME: this.name,
+                MESSASGE: this.message
+            })
+        },
     },
 
-    onResponse(error, data) {
-      this.loading = false
+    methods: {
+        setEmail(value = '') {
+            this.email = value.trim()
+        },
+        setName(value = '') {
+            this.name = value.trim()
+        },
+        setMessage(value = '') {
+            this.messsage = value.trim()
+        },
 
-      if (error) {
-        this.error = error
-      }
+        subscribe() {
+            if (this.email === null || this.name === null || this.message === null || this.loading) {
+                return
+            }
 
-      if (data && data.result === 'error') {
-        this.error = this.formatErrorMessage(data.msg)
-      }
+            this.success = false
+            this.error = null
+            this.loading = true
 
-      if (this.error) {
-        this.$emit('error', this.error)
-      } else {
-        this.success = true
-        this.email = null
-        this.$emit('success')
-      }
+            const url = `${this.url}?${this.data}`
+
+            jsonp(url, { param: 'c' }, this.onResponse)
+        },
+
+        onResponse(error, data) {
+            this.loading = false
+
+            if (error) {
+                this.error = error
+            }
+
+            if (data && data.result === 'error') {
+                this.error = this.formatErrorMessage(data.msg)
+            }
+
+            if (this.error) {
+                this.$emit('error', this.error)
+            } else {
+                this.success = true
+                this.email = null
+                this.name = null
+                this.message = null
+                this.$emit('success')
+            }
+        },
+
+        formatErrorMessage(message) {
+            return message.replace('0 - ', '')
+        },
     },
 
-    formatErrorMessage(message) {
-      return message.replace('0 - ', '')
+    render() {
+        return this.$scopedSlots.default({
+            subscribe: this.subscribe,
+            setEmail: this.setEmail,
+            setName: this.setName,
+            setMessage: this.setMessage,
+            error: this.error,
+            success: this.success,
+            loading: this.loading,
+        })
     },
-  },
-
-  render() {
-    return this.$scopedSlots.default({
-      subscribe: this.subscribe,
-      setEmail: this.setEmail,
-      error: this.error,
-      success: this.success,
-      loading: this.loading,
-    })
-  },
 }
